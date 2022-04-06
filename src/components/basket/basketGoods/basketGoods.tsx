36fd955994554b82a8c27d1eade099674ca129e2
Andrey Zhilin <peefagor@gmail.com>
2022-04-06 23:56:41 +0300

    add plus and minus icon

--- src/components/basket/basketGoods/basketGoods.tsx
+++ src/components/basket/basketGoods/basketGoods.tsx
@@ -1,6 +1,8 @@
 import style from "./basketGoods.module.css"
 import img from '../../../../public/img/catalog_img.jpg'
 import close from '../../../../public/img/close_icon.svg'
+import plus from '../../../../public/img/plus_icon.svg'
+import minus from '../../../../public/img/minus_icon.svg'
 
 export function BasketGoods(){
     return(
@@ -16,6 +18,15 @@ export function BasketGoods(){
                 </div>
                 <div className={style.cost}>
                     <p className={style.price}>125000 <span data-rub>₽</span></p>
+                    <div className={style.nav}>
+                        <div className={style.nav_icon}>
+                            <img src={minus.src} alt="" />
+                        </div>
+                        <p className={style.nav_txt}>1</p>
+                        <div className={style.nav_icon}>
+                            <img src={plus.src} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
